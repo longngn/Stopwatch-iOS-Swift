0421e6ea9f2104ca9c9e54f65cdeb7b9b009a01f
Nguyen Le Vu Long <nguyenlevulong@Nguyens-MacBook-Pro.local>
2016-12-23 22:16:46 +0700

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Nguyen Le Vu Long on 12/23/16.
+//  Copyright © 2016 Nguyen Le Vu Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
